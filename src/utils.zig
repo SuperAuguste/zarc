25b959c94566587ebe05e8609f6e7d57af469886
Nameless <truemedian@gmail.com>
2021-08-28 09:56:41 -0500

    make zip parser generic, allow plucking files out of archive

--- src/utils.zig
+++ src/utils.zig
@@ -2,11 +2,12 @@ const std = @import("std");
 
 pub const simd = @import("utils/simd.zig");
 
-pub fn readStruct(comptime T: type, instance: *T, reader: anytype, comptime expected_size: usize) !usize {
+pub fn readStruct(comptime T: type, instance: *T, reader: anytype, comptime expected_size: usize) !void {
     const fields = std.meta.fields(T);
 
     var buf: [expected_size]u8 = undefined;
-    _ = try reader.readAll(&buf);
+    const read = try reader.readAll(&buf);
+    if (read != expected_size) return error.EndOfStream;
 
     comptime var index = 0;
     inline for (fields) |field| {
@@ -20,9 +21,9 @@ pub fn readStruct(comptime T: type, instance: *T, reader: anytype, comptime expe
                     @field(instance, field.name) = @bitCast(field.field_type, std.mem.readIntLittle(Int, buf[index..][0..@sizeOf(Int)]));
                     index += @sizeOf(Int);
                 } else {
-                    const Int = @typeInfo(@TypeOf(child.cast)).Fn.args[0].arg_type.?;
+                    const Int = @typeInfo(@TypeOf(child.read)).Fn.args[0].arg_type.?;
 
-                    @field(instance, field.name) = child.cast(std.mem.readIntLittle(Int, buf[index..][0..@sizeOf(Int)]));
+                    @field(instance, field.name) = child.read(std.mem.readIntLittle(Int, buf[index..][0..@sizeOf(Int)]));
                     index += @sizeOf(Int);
                 }
             },
@@ -45,6 +46,39 @@ pub fn readStruct(comptime T: type, instance: *T, reader: anytype, comptime expe
     }
 
     if (index != expected_size) @compileError("invalid size");
+}
 
-    return expected_size;
+pub fn LimitedReader(comptime ReaderType: type) type {
+    return struct {
+        unlimited_reader: ReaderType,
+        limit: usize,
+        pos: usize = 0,
+
+        pub const Error = ReaderType.Error;
+        pub const Reader = std.io.Reader(*Self, Error, read);
+
+        const Self = @This();
+
+        pub fn init(unlimited_reader: ReaderType, limit: usize) Self {
+            return .{
+                .unlimited_reader = unlimited_reader,
+                .limit = limit,
+            };
+        }
+
+        fn read(self: *Self, dest: []u8) Error!usize {
+            if (self.pos >= self.limit) return 0;
+
+            const left = std.math.min(self.limit - self.pos, dest.len);
+            const num_read = try self.unlimited_reader.read(dest[0..left]);
+
+            self.pos += num_read;
+
+            return num_read;
+        }
+
+        pub fn reader(self: *Self) Reader {
+            return .{ .context = self };
+        }
+    };
 }
--- tests/zip.zig
+++ tests/zip.zig
@@ -25,6 +25,9 @@ pub fn main() !void {
     var tests_dir = try std.fs.cwd().openDir("tests/zip", .{ .iterate = true });
     defer tests_dir.close();
 
+    var main_extract_dir = try std.fs.cwd().makeOpenPath("tests/extract/zip", .{});
+    defer main_extract_dir.close();
+
     var report = try std.fs.cwd().createFile("tests/zip.report.txt", .{});
     defer report.close();
 
@@ -35,26 +38,42 @@ pub fn main() !void {
         var archive_file = try tests_dir.openFile(entry.name, .{});
         defer archive_file.close();
 
+        var extract_dir = try main_extract_dir.makeOpenPath(entry.name, .{});
+        defer extract_dir.close();
+
         const size = try archive_file.getEndPos();
 
         var timer = try std.time.Timer.start();
-        var archive = zarc.zip.Parser.init(allocator, archive_file);
+        var archive = zarc.zip.Parser(std.fs.File.Reader).init(allocator, archive_file.reader());
         defer archive.deinit();
+
+        try writer.print("File: {s}\n", .{entry.name});
 
         try archive.load();
         const time = timer.read();
 
-        try writer.print("File: {s}\n", .{entry.name});
-        try writer.print("Runtime: {d:.3}ms\n\n", .{@intToFloat(f64, time) / 1e6});
+        const load_time = @intToFloat(f64, time) / 1e9;
+        const read_speed = (@intToFloat(f64, archive.ecd.directory_size) * 2 + @intToFloat(f64, archive.directory_offset)) / load_time;
+
+        try writer.print("Runtime: {d:.3}ms\n\n", .{load_time * 1e3});
+        try writer.print("Speed: {d:.3} MB/s\n", .{read_speed / 1e6});
         try writer.print("Total Size: {d}\n", .{size});
         try writer.print("Offset: {d}\n", .{archive.start_offset});
-        try writer.print("Archive Size: {d}\n", .{size - archive.start_offset});
-        try writer.print("ZIP64: {}\n", .{archive.is_zip64});
+        try writer.print("Directory Size: {d}\n", .{archive.ecd.directory_size});
+        try writer.print("Directory Offset: {d}\n", .{archive.directory_offset});
         try writer.print("Entries: {d}\n", .{archive.num_entries});
-        try writer.print("Directory Size: {d}\n", .{archive.dir_size});
-        try writer.print("Directory Offset: {d}\n\n", .{archive.dir_offset});
+        try writer.print("ZIP64: {}\n", .{archive.is_zip64});
 
-        try printFileTree(writer, archive.file_tree);
+        // try printFileTree(writer, try archive.getFileTree());
+
+        // const start = timer.read();
+        // const total_written = try archive.extract(extract_dir, .{ .skip_components = 1 });
+        // const stop = timer.read();
+
+        // const extract_time = @intToFloat(f64, stop - start) / 1e9;
+        // const extract_speed = @intToFloat(f64, total_written) / extract_time;
+        // try writer.print("Extract Size: {d}\n", .{total_written});
+        // try writer.print("Extract Speed: {d:.3} MB/s\n", .{extract_speed / 1e6});
 
         try writer.writeAll("\n-----\n\n");
     }
